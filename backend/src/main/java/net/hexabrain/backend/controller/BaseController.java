7ee162769e04f97015a4dbf78d173b03239fe51d
layer6ai <layer6ai@gmail.com>
2022-07-13 22:36:59 +0900

    first commit

---
+++ backend/src/main/java/net/hexabrain/backend/controller/BaseController.java
@@ -0,0 +1,13 @@
+package net.hexabrain.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
